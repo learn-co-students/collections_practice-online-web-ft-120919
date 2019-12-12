d78101c4f44da7fcd32d5b714b4de4cdc608d33f
Erik Kasperson <chefkasperson@gmail.com>
2019-12-12 03:37:34 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,3 +9,20 @@ end
 def sort_array_char_count(array)
   array.sort_by {|x| x.length}
 end
+
+def swap_elements(array)
+  array.insert(1, array.delete_at(2))
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  arr = []
+  array.each {|e|
+    e[2] = "$"
+    arr << e
+    }
+    arr
+end
