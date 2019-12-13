beeafe583a34d66218e02c054609382b92803862
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-13 00:41:11 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+def sort_array_asc (array)
+  array.sort 
+end
 
+def sort_array_desc (array)
+  array.sort.reverse
+end
+
+def sort_array_char_count (array)
+  array.sort { |a, b| a.length <=> b.length }
+end
+
+def swap_elements (array)
+  array.insert(2,array.delete_at(1))
+end
+
+def reverse_array (array)
+  array.reverse
+end
+
+def kesha_maker (array)
+  new_array = []
+  array.each do |word|
+    new_word = word.delete(word[2])
+    new_array << new_word.insert(2,"$")
+  end
+  new_array
+end
+
+def find_a (array)
+  array.select { |word| word.start_with? "a"}
+end
+
+def sum_array (array)
+  array.inject { |sum, num| sum + num }
+end
+
+def add_s (array)
+  array.each_with_index.collect do |element, index| 
+      unless index == 1 
+        element + "s"
+      else
+        element
+    end
+  end
+end
+  
+  
+  
+  
+  
+  
+  
