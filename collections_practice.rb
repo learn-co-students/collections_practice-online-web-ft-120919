f8bb8fe59d484c8821c1af0047cfab411949b595
github username <github email address>
2019-12-13 23:41:24 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
+require 'pry'
 
+def sort_array_asc(array)
+    array.sort
+end
+
+def sort_array_desc(array)
+    array.sort.reverse
+end
+
+def sort_array_char_count(array)
+    array.sort_by do|animal|
+        animal.length
+    end
+end
+
+def swap_elements(array)
+    array[0], array[1], array[2] = array[0],array[2], array[1]
+end
+
+# def swap_elements_from_to(array, index, destination_index)
+
+# end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each do |names|
+        names[2] = "$"
+    end
+end
+
+def find_a(array)
+    array.select do |words|
+        words.start_with?("a")
+    end
+end
+
+def sum_array(array)
+    array.sum
+end
+
+def add_s(array)
+    array.each { |word|
+    # binding.pry
+    word << "s"
+    # word[1].pop
+    }
+end
